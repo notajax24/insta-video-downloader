6c20f30aa7348736b442e49332cf55f72bd71146
notajax24 <96714712+notajax24@users.noreply.github.com>
2024-11-18 16:19:39 +0530

    commit

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -15,8 +15,7 @@ const Home = () => {
   const [loading, setLoading] = useState(false); // Track loading state
   const [progress, setProgress] = useState(0);
 
-  const BACKEND_URL =
-    import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+  const BACKEND_URL = "https://insta-video-downloader-backend.vercel.app";
 
   // Function to handle paste button click
   const pasteFromClipboard = async () => {
